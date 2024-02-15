294db5b04f908cca904f85d3d75e0f7a28de9379
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-02-15 14:48:53 +0200

    Add project files.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            BirthDate birthDate = new BirthDate(new DateTime(1900, 11, 10));
+            Console.WriteLine(birthDate.GetDaysUntilBirthDay());
+        }
+    }
+    struct BirthDate
+    {
+        private DateTime date;
+        public BirthDate(DateTime date)
+        {
+            this.date = date;
+        }
+        public string GetWeekdayOfBirth()
+        {
+            return date.ToString("___");
+        }
+        public string GetWeekdayOfBirthInYear(int year)
+        {
+            DateTime date2 = new DateTime(year, date.Month, date.Day);
+            return date2.ToString("___");
+        }
+        public int GetDaysUntilBirthDay()
+        {
+            DateTime today = DateTime.Today;
+            DateTime nextBirth = new DateTime(today.Year, date.Month, date.Day);
+            if (nextBirth < today)
+            {
+                nextBirth = nextBirth.AddYears(1);
+            }
+            return (nextBirth - today).Days;
+        }
+    }
+}
